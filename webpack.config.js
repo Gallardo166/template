8d4a25916060d93f0928fa5e1fe0d1a6f2ffae32
Gallardo Lee <leegallardo16@gmail.com>
2024-04-08 01:24:40 +0700

    Install style and css loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      filename: "index.html",
+      filename: "./index.html",
       inject: "head",
       scriptLoading: "defer",
     }),
